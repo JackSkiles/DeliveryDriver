09a12e455166dc73ec71b05c4aa50a28c601e7a6
Jack Skiles <jackskiles420@gmail.com>
2021-11-29 21:44:05 -0500

    added new component to handle collision and with it some random phrases

---
+++ Collision.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Collision : MonoBehaviour
+{
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        string[] phrases = new string[6];
+        phrases[0] = "Damn!";
+        phrases[1] = "That's the last time I drink and drive";
+        phrases[2] = "This is worse than the time.";
+        phrases[3] = "I hope that was a speed bump.";
+        phrases[4] = "Yow!";
+        phrases[5] = "Can't think of anything more to say.";
+        int phrase = Random.Range(0, 6);
+        Debug.Log(phrases[phrase]);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("PassedTrigger");  
+    }
+}
